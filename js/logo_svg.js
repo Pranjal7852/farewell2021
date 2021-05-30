2955c3ae9113ad60a53ee128814a1c54f5763a0a
Pranjal Goyal <id4pranjal@gmail.com>
2021-05-30 15:49:53 +0530

    vomi sir commit

--- js/logo_svg.js
+++ js/logo_svg.js
@@ -1,4 +1,104 @@
+// GAP
 var imagediv1 = document.querySelector("#s1");
 imagediv1.addEventListener("mouseenter", function() {
-    document.querySelector("#t1").classList.toggle("show");
+    document.querySelector("#t1").style.display = "block";
+})
+imagediv1.addEventListener("mouseleave", function() {
+    document.querySelector("#t1").style.display = "none";
+})
+// GAP
+var imagediv2 = document.querySelector("#s2");
+imagediv2.addEventListener("mouseenter", function() {
+    document.querySelector("#t2").style.display = "block";
+})
+imagediv2.addEventListener("mouseleave", function() {
+    document.querySelector("#t2").style.display = "none";
+})
+// GAP
+var imagediv3 = document.querySelector("#s3");
+imagediv3.addEventListener("mouseenter", function() {
+    document.querySelector("#t3").style.display = "block";
+})
+imagediv3.addEventListener("mouseleave", function() {
+    document.querySelector("#t3").style.display = "none";
+})
+// GAP
+var imagediv1 = document.querySelector("#s1");
+imagediv1.addEventListener("mouseenter", function() {
+    document.querySelector("#t1").style.display = "block";
+})
+imagediv1.addEventListener("mouseleave", function() {
+    document.querySelector("#t1").style.display = "none";
+})
+// GAP
+var imagediv1 = document.querySelector("#s1");
+imagediv1.addEventListener("mouseenter", function() {
+    document.querySelector("#t1").style.display = "block";
+})
+imagediv1.addEventListener("mouseleave", function() {
+    document.querySelector("#t1").style.display = "none";
+})
+// GAP
+var imagediv1 = document.querySelector("#s1");
+imagediv1.addEventListener("mouseenter", function() {
+    document.querySelector("#t1").style.display = "block";
+})
+imagediv1.addEventListener("mouseleave", function() {
+    document.querySelector("#t1").style.display = "none";
+})
+// GAP
+var imagediv1 = document.querySelector("#s1");
+imagediv1.addEventListener("mouseenter", function() {
+    document.querySelector("#t1").style.display = "block";
+})
+imagediv1.addEventListener("mouseleave", function() {
+    document.querySelector("#t1").style.display = "none";
+})
+// GAP
+var imagediv1 = document.querySelector("#s1");
+imagediv1.addEventListener("mouseenter", function() {
+    document.querySelector("#t1").style.display = "block";
+})
+imagediv1.addEventListener("mouseleave", function() {
+    document.querySelector("#t1").style.display = "none";
+})
+// GAP
+var imagediv1 = document.querySelector("#s1");
+imagediv1.addEventListener("mouseenter", function() {
+    document.querySelector("#t1").style.display = "block";
+})
+imagediv1.addEventListener("mouseleave", function() {
+    document.querySelector("#t1").style.display = "none";
+})
+// GAP
+var imagediv1 = document.querySelector("#s1");
+imagediv1.addEventListener("mouseenter", function() {
+    document.querySelector("#t1").style.display = "block";
+})
+imagediv1.addEventListener("mouseleave", function() {
+    document.querySelector("#t1").style.display = "none";
+})
+// GAP
+var imagediv1 = document.querySelector("#s1");
+imagediv1.addEventListener("mouseenter", function() {
+    document.querySelector("#t1").style.display = "block";
+})
+imagediv1.addEventListener("mouseleave", function() {
+    document.querySelector("#t1").style.display = "none";
+})
+// GAP
+var imagediv1 = document.querySelector("#s1");
+imagediv1.addEventListener("mouseenter", function() {
+    document.querySelector("#t1").style.display = "block";
+})
+imagediv1.addEventListener("mouseleave", function() {
+    document.querySelector("#t1").style.display = "none";
+})
+// GAP
+var imagediv1 = document.querySelector("#s1");
+imagediv1.addEventListener("mouseenter", function() {
+    document.querySelector("#t1").style.display = "block";
+})
+imagediv1.addEventListener("mouseleave", function() {
+    document.querySelector("#t1").style.display = "none";
 })
